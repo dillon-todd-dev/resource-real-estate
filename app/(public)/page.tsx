3c81743f21c48efa7bfd141fc9e91671022c50fa
dillon-todd-dev <dillontodd.dev@gmail.com>
2025-08-30 11:03:02 -0500

    add shadcn

---
+++ app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1>Welcome to Resource Real Estate</h1>
+      <p>Real estate investment made simple.</p>
+    </div>
+  );
+}
